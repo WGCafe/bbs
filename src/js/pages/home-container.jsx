ffbd6a1cd93232efc7f93de7b83e6b36b8b84ef1
wendylin <wendylw2013@gmail.com>
2018-11-17 20:15:48 +0800

    Add homepage prototype page

---
+++ src/js/pages/home-container.jsx
@@ -0,0 +1,19 @@
+import {Layout} from 'antd';
+import React, {Component} from 'react';
+
+import '../styles/common.less';
+
+class HomeContainer extends Component {
+  componentWillMount() {}
+
+  componentWillReceiveProps() {}
+
+  render() {
+    return (
+      <Layout className="">
+      </Layout>
+    );
+  }
+}
+
+HomeContainer.PropTypes = {};

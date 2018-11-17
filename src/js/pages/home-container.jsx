668e1410c42f35e2b378b8cb42c43ad015abf9b0
wendylin <wendylw2013@gmail.com>
2018-11-17 22:08:33 +0800

    Update layout

--- src/js/pages/home-container.jsx
+++ src/js/pages/home-container.jsx
@@ -22,22 +22,22 @@ class HomeContainer extends Component {
             theme="dark"
             mode="horizontal"
             defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
+            style={{lineHeight: '64px'}}
           >
             <Menu.Item key="1">nav 1</Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
+        <Content style={{padding: '0 50px'}}>
+          <Breadcrumb style={{margin: '16px 0'}}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+          <div style={{background: '#fff', padding: 24, minHeight: 280}}>Content</div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        <Footer style={{textAlign: 'center'}}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>

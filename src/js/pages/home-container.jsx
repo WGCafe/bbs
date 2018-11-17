8519977eae3a38dcf80dc8ea6a83ecb3b78c9bbd
wendylin <wendylw2013@gmail.com>
2018-11-17 22:08:26 +0800

    Update git s

--- src/js/pages/home-container.jsx
+++ src/js/pages/home-container.jsx
@@ -1,4 +1,4 @@
-import {Layout} from 'antd';
+import {Layout, Menu, Breadcrumb} from 'antd';
 import React, {Component} from 'react';
 import '../../styles/common.less';
 
@@ -18,11 +18,26 @@ class HomeContainer extends Component {
       <Layout className="layout">
         <Header>
           <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
         </Header>
-        <Content style={{padding: '0 50px'}}>
-          <div style={{background: '#fff'}}>Content</div>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
         </Content>
-        <Footer style={{textAlign: 'center'}}>
+        <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
       </Layout>

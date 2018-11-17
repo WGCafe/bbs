a61b0cba913dba86a64b4f5658d402108d91e378
wendylin <wendylw2013@gmail.com>
2018-11-17 19:57:23 +0800

    Styles: Change stylus to less

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Routers from './routers.jsx';
-import '../styles/common.styl';
+import '../styles/common.less';
 
 ReactDOM.render(
   <div className="app">

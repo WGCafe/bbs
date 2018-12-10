46ad5f1d29bd1b5340d81053e3ecf921ee7b0348
wendylin <wendylw2013@gmail.com>
2018-12-10 23:27:02 +0800

    Update post list logic

--- src/js/components/post-list.jsx
+++ src/js/components/post-list.jsx
@@ -19,39 +19,42 @@ class PostList extends Component {
         className={isAll ? 'post__avatar-list' : ''}
         itemLayout="horizontal"
         dataSource={postList}
-        renderItem={item => (
-          <List.Item className="post__item">
-            <div className="ant-list-item-meta post__item-meta">
-              {
-                isAll ? (
-                  <div className="ant-list-item-meta-avatar">
-                    <Avatar size={52} icon="user" />
-                  </div>
-                ) : null
-              }
-              {{/*<div className="post__rate">
-                <Icon type="caret-up" />
-                <span className="post__rate-number">200</span>
-                <Icon type="caret-down" />
-              </div>*/}}
-              <div className="ant-list-item-meta-content">
-                <h4 className="ant-list-item-meta-title"><a className="post__item-title" href="">{item.title}</a></h4>
-                <div className="ant-list-item-meta-description">
-                  <div className="post__item-info">
-                    <span className="text-middle">{item.comments_num} 回复</span>
-                    <i className="text-middle">•</i>
-                    <span className="text-middle">{item.comments_num} 小时前</span>
-                    <span className="text-middle">来自 {item.author}</span>
+        renderItem={item => {
+          const {author} = item;
+
+          return (
+            <List.Item className="post__item">
+              <div className="ant-list-item-meta post__item-meta">
+                {
+                  isAll ? (
+                    <div className="ant-list-item-meta-avatar">
+                      <Avatar size={52} src={author.face_image} />
+                    </div>
+                  ) : null
+                }
+                {{/*<div className="post__rate">
+                  <Icon type="caret-up" />
+                  <span className="post__rate-number">200</span>
+                  <Icon type="caret-down" />
+                </div>*/}}
+                <div className="ant-list-item-meta-content">
+                  <h4 className="ant-list-item-meta-title"><a className="post__item-title" href="">{item.title}</a></h4>
+                  <div className="ant-list-item-meta-description">
+                    <div className="post__item-info">
+                      <span className="text-middle">{item.comments_num} 回复</span>
+                      <i className="text-middle">•</i>
+                      <span className="text-middle">{item.update_time} 小时前</span>
+                      <span className="text-middle">来自 {author.nick_name}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="ant-list-item-extra">
-              <Icon type="star" theme="filled" />
-            </div>
-          </List.Item>
-        )}
-      />
+              <div className="ant-list-item-extra">
+                <Icon type="star" theme="filled" />
+              </div>
+            </List.Item>
+          );
+        }}/>
     );
   }
 }
--- src/js/pages/home-container.jsx
+++ src/js/pages/home-container.jsx
@@ -8,44 +8,84 @@ import PostList from '../components/post-list.jsx';
 const {TabPane} = Tabs;
 
 class HomeContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeKey: "0"
+    };
+
+    this.handleTabClick = this.handleTabClick.bind(this);
+  }
+
+  fetchCurrentPostList() {
+    //TODO add active key list action
+  }
+
+  handleTabClick(activeKey) {
+    this.fetchCurrentPostList();
+    this.setState({activeKey});
+  }
+
+  renderTabPane(tabOptions) {
+    if (!tabOptions) {
+      return null;
+    }
+
+    const {postList} = this.props;
+    const {activeKey} = this.state;
+
+    return (
+      <TabPane tab={tabOptions.name} key={tabOptions.currentKey}>
+        <div className="post__container container">
+          <Row type="flex" justify="space-between" align="top" gutter={24}>
+            <Col span={17}>
+              {
+                activeKey === tabOptions.currentKey ? (
+                  <PostList postList={postList} isAll={true}/>
+                ) : null
+              }
+            </Col>
+            <Col span={7}>
+              {/* <SideList/> */}
+            </Col>
+          </Row>
+        </div>
+      </TabPane>
+    );
+  }
+
   render() {
     const {tabs} = this.props;
+    const {activeKey} = this.state;
 
     return (
       <LayoutContainer>
-        <Tabs className="sub-header" defaultActiveKey="0" animated={false}>
-          <TabPane tab="全部" key="0">
-            <div className="post__container container">
-              <Row type="flex" justify="space-between" align="top" gutter={24}>
-                <Col span={17}>
-                  <PostList hasAvatar={true}/>
-                </Col>
-                <Col span={7}>
-                  {/* <SideList/> */}
-                </Col>
-              </Row>
-            </div>
-          </TabPane>
+        <Tabs
+          className="sub-header"
+          activeKey={activeKey}
+          animated={false}
+          onTabClick={this.handleTabClick}
+        >
+          {this.renderTabPane({
+            name: "全部",
+            currentKey: "0"
+          })}
           {
             tabs.map((tab) => {
               const key = tab.id;
+              const currentKey = key.toString();
 
-              return (
-                <TabPane tab={tab.name} key={key.toString()}>
-                  <div className="post__container container">
-                    <Row type="flex" justify="space-between" align="top" gutter={24}>
-                      <Col span={17}>
-                        <PostList hasAvatar={true}/>
-                      </Col>
-                      <Col span={7}>
-                        {/* <SideList/> */}
-                      </Col>
-                    </Row>
-                  </div>
-                </TabPane>
-              );
+              return this.renderTabPane({
+                name: tab.name,
+                currentKey
+              });
             })
           }
+          {this.renderTabPane({
+            name: "收藏的话题",
+            currentKey: (tabs.length + 1).toString()
+          })}
         </Tabs>
       </LayoutContainer>
     );
@@ -56,7 +96,8 @@ HomeContainer.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string
-  }))
+  })),
+  postList: PropTypes.arrayOf(PropTypes.object)
 };
 
 HomeContainer.defaultProps = {
@@ -83,7 +124,8 @@ HomeContainer.defaultProps = {
   {
     id: 6,
     name: "种植"
-  }]
+  }],
+  postList: []
 };
 
 export default HomeContainer;

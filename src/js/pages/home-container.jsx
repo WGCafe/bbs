41c5fc31a54dc8a123fe992922f483675854595f
wendylin <wendylw2013@gmail.com>
2018-12-10 22:30:55 +0800

    Update home container tabs data

--- src/js/pages/home-container.jsx
+++ src/js/pages/home-container.jsx
@@ -14,10 +14,24 @@ class HomeContainer extends Component {
     return (
       <LayoutContainer>
         <Tabs className="sub-header" defaultActiveKey="0" animated={false}>
+          <TabPane tab="全部" key="0">
+            <div className="post__container container">
+              <Row type="flex" justify="space-between" align="top" gutter={24}>
+                <Col span={17}>
+                  <PostList hasAvatar={true}/>
+                </Col>
+                <Col span={7}>
+                  {/* <SideList/> */}
+                </Col>
+              </Row>
+            </div>
+          </TabPane>
           {
             tabs.map((tab) => {
+              const key = tab.id;
+
               return (
-                <TabPane tab={tab.name} key={tab.id}>
+                <TabPane tab={tab.name} key={key.toString()}>
                   <div className="post__container container">
                     <Row type="flex" justify="space-between" align="top" gutter={24}>
                       <Col span={17}>
@@ -39,10 +53,37 @@ class HomeContainer extends Component {
 }
 
 HomeContainer.propTypes = {
-  tabs: PropTypes.shape({
-    id: PropTypes.string,
+  tabs: PropTypes.arrayOf({
+    id: PropTypes.number,
     name: PropTypes.string
   })
+};
+
+HomeContainer.defaultProps = {
+  tabs: [{
+    id: 1,
+    name: "讨论"
+  },
+  {
+    id: 2,
+    name: "询问"
+  },
+  {
+    id: 3,
+    name: "记录"
+  },
+  {
+    id: 4,
+    name: "牙套"
+  },
+  {
+    id: 5,
+    name: "智齿"
+  },
+  {
+    id: 6,
+    name: "种植"
+  }]
 };
 
 export default HomeContainer;
